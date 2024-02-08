41d958ebc26f7aeb7068867a1d77d3424ed604aa
Thisumitha <thisu2006@gmail.com>
2024-02-08 22:03:38 +0530

    Login and Register Forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './common/nav/nav.component';
 import { ViewAllBooksComponent } from "./pages/view-all-books/view-all-books.component";
+import { LoginComponent } from './pages/login/login.component';
 
 
 @Component({
@@ -9,7 +10,7 @@ import { ViewAllBooksComponent } from "./pages/view-all-books/view-all-books.com
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, NavComponent, ViewAllBooksComponent]
+    imports: [RouterOutlet,NavComponent]
 })
 export class AppComponent {
   title = 'library-management-front-end-main';
--- src/app/common/nav/nav.component.ts
+++ src/app/common/nav/nav.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { LoginComponent } from '../../pages/login/login.component';
+import { ViewAllBooksComponent } from '../../pages/view-all-books/view-all-books.component';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
